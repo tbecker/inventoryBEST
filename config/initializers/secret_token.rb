2ee769fa9a676a9d1ba71f57c4ea2a662f458ba2
Torsten Becker <tbecker@best-group.eu>
2011-06-22 20:30:20 +0200

    initial start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InventoryBEST::Application.config.secret_token = '70bb1b9b03fa2917ec6dea5c983404d2ac349b24631dd4f62fa50cea7f3909487ed2cc15e2fafa688c62669b3303fa6d87f8d1bf540ba8e268c2388641b83745'
